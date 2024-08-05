7ad026c0b25597288a3e9a024aa052cd9cbb95a1
fluidmotion-sys <julian@kiesenhofer.net>
2024-08-05 05:57:15 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,26 +1,16 @@
-import plotly.express as px
-import pandas as pd
+import csv
 import numpy as np
-from IPython.display import display
-df = pd.read_csv('heat_map_data.csv')
-j = 0
-b = 0
-z = [[ 3,  3,  1,  2,  3,  4],  #float(a.split()[1])
-     [ 5,  6,  7,  8,  9, 10],
-     [11, 12, 13, 14, 15, 16],
-     [17, 18, 19, 20, 21, 22],
-     [23, 24, 25, 26, 27, 28],
-     [29, 30, 31, 32, 33, 34]]
-y=z
-l=0
-#display(df)
-for x in df.to_numpy():
-  a = str(df.to_numpy()[j])
-  b = float(a.split()[1])
-  #print("map =", b)
-  j += 1
-  for l in range(6-l):
-      y = z[l][j]
-  #print("y = ", y)
-fig = px.imshow(z)
+import plotly.express as px
+d=np.zeros((6,6))
+k=0
+with open('heat_map_data.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        #print(len(str(row)))
+        b=str(row)[3:104]
+        c=b.split()
+        for i in range(len(c)):
+            d[k,i]=c[i]
+        k+=1
+fig = px.imshow(d,text_auto=True)
 fig.show()
